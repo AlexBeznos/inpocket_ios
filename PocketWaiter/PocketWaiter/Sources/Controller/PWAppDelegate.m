1b017ed3eb74a0fa8e23861b263058cc35d1ffeb
nbotyrj1 <mishatishenko@gmail.com>
2016-09-05 22:41:38 +0300

    Add fb sdk

--- PocketWaiter/PocketWaiter/Sources/Controller/PWAppDelegate.m
+++ PocketWaiter/PocketWaiter/Sources/Controller/PWAppDelegate.m
@@ -8,6 +8,7 @@
 
 #import "PWAppDelegate.h"
 #import <GoogleMaps/GoogleMaps.h>
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
 
 @interface PWAppDelegate ()
 
@@ -19,8 +20,23 @@
 			didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
 	[GMSServices provideAPIKey:@"AIzaSyBey584OKFmQxy25R_b_K_av16_wxHdvYY"];
+	[[FBSDKApplicationDelegate sharedInstance] application:application
+                           didFinishLaunchingWithOptions:launchOptions];
 	
 	return YES;
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application
+{
+	[FBSDKAppEvents activateApp];
+}
+
+
+- (BOOL)application:(UIApplication *)application  openURL:(NSURL *)url
+			sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
+{
+	return [[FBSDKApplicationDelegate sharedInstance] application:application
+				openURL:url sourceApplication:sourceApplication annotation:annotation];
 }
 
 
