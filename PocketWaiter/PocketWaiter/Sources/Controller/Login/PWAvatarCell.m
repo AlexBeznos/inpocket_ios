903819594114a012c35ef9935b764de58ed5e766
Eugene Aronov <aronov233@gmail.com>
2017-01-19 14:10:24 +0200

    changed area where to tap to change avatar

--- PocketWaiter/PocketWaiter/Sources/Controller/Login/PWAvatarCell.m
+++ PocketWaiter/PocketWaiter/Sources/Controller/Login/PWAvatarCell.m
@@ -13,17 +13,24 @@
 
 @property (strong, nonatomic) IBOutlet PWImageView *avatarView;
 @property (strong, nonatomic) IBOutlet UILabel *title;
+@property (weak, nonatomic) IBOutlet UIView *cellView;
 
 @end
 
 @implementation PWAvatarCell
 
-- (IBAction)changeAvatar:(id)sender
-{
-	if (nil != self.handler)
-	{
-		self.handler();
-	}
+- (IBAction)changeAvatar:(id)sender {
+    if (nil != self.handler)
+    {
+        self.handler();
+    }
+}
+
+- (void)awakeFromNib{
+    [super awakeFromNib];
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget: self action: @selector(changeAvatar:)];
+    [_cellView addGestureRecognizer:tap];
+    
 }
 
 @end
